a2092b426eda8b047d834979e5f47f24aebf246c
Burcu <burcumantar0@gmail.com>
2023-09-14 02:37:59 +0300

    .

--- PatientManagementAndAppointmentSystem_GF/Interfaces/IHistoryService.cs
+++ PatientManagementAndAppointmentSystem_GF/Interfaces/IHistoryService.cs
@@ -1,6 +1,13 @@
-﻿namespace PatientManagementAndAppointmentSystem_GF.Interfaces
+﻿using PatientManagementAndAppointmentSystem_GF.Entities;
+
+namespace PatientManagementAndAppointmentSystem_GF.Interfaces
 {
     public interface IHistoryService
     {
+        Task<MedicalHistory> GetById(int id);
+        Task<List<MedicalHistory>> ListAll();
+        Task<MedicalHistory> Add(MedicalHistory entity);
+        Task Update(MedicalHistory entity);
+        Task Delete(MedicalHistory entity);
     }
 }
